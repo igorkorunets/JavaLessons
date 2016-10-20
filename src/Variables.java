3ca44d0ace8a1dc4c586fbecb755382e814a4c83
Igor Korunets <igor.korounets@gmail.com>
2016-10-20 20:56:10 +0300

    add Variables

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+/*
+Project name: "Переменные. Примитивные типы данных"
+13.10.2016
+*/
+public class Variables {
+  public static void main(String[] args) {
+    int myInt;
+    myInt = 557;
+    System.out.println(myInt);
+  }
+}
