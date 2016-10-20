67069194f2a0d20ab7a2daaba0db2ceb932ee68d
Igor Korunets <igor.korounets@gmail.com>
2016-10-21 00:00:43 +0300

    Whileloops - 1change

--- src/Whileloops.java
+++ src/Whileloops.java
@@ -4,16 +4,23 @@ Project name: "Цикл While"
 */
 public class Whileloops {
   public static void main(String[] args) {
+    int value = 0;
     boolean a = true;
     boolean b = 5 > 2;
     boolean c = 5 > 7;
     boolean d = 5 >= 5;
     boolean e = 5 == 1;
+    boolean f = value > 5;
+    while(value < 5) {
+      System.out.println("Hello " + value);
+      value++;
+    }
 
     System.out.println(a);
     System.out.println(b);
     System.out.println(c);
     System.out.println(d);
     System.out.println(e);
+    System.out.println(f);
   }
 }
