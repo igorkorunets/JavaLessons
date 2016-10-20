3eb3ca2e73fe180a714f8b077371f6432e68f63a
Igor Korunets <igor.korounets@gmail.com>
2016-10-20 23:45:43 +0300

    add Whileloops

---
+++ src/Whileloops.java
@@ -0,0 +1,19 @@
+/*
+Project name: "Цикл While"
+14.10.2016
+*/
+public class Whileloops {
+  public static void main(String[] args) {
+    boolean a = true;
+    boolean b = 5 > 2;
+    boolean c = 5 > 7;
+    boolean d = 5 >= 5;
+    boolean e = 5 == 1;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+  }
+}
