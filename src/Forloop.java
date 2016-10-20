4037f2e317b769db1539d8b05d16d1c38cf6647f
Igor Korunets <igor.korounets@gmail.com>
2016-10-21 00:16:32 +0300

    add Forloop

---
+++ src/Forloop.java
@@ -0,0 +1,11 @@
+/*
+Project name: "Цикл For"
+21.10.2016
+*/
+public class Forloop {
+  public static void main(String[] args) {
+    for(int i = 0; i <= 10; i++) {
+      System.out.println("Hello " + i);
+    }
+  }
+}
