518ed4b2497343a22c2935adca98245bd26cf246
Igor Korunets <igor.korounets@gmail.com>
2016-10-21 00:26:01 +0300

    add If

---
+++ src/If.java
@@ -0,0 +1,13 @@
+/*
+Project name: "Условный оператор if"
+21.10.2016
+*/
+public class If {
+  public static void main(String[] args) {
+    if(5 < 4) {
+      System.out.println("Да, верно!");
+    } else {
+      System.out.println("Нет, неверно!");
+    }
+  }
+}
