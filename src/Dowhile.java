c49c8486905ed8b53be5fe14c1fa6df2360119b8
Igor Korunets <igor.korounets@gmail.com>
2016-10-21 14:30:07 +0300

    Dowhile - 1change

--- src/Dowhile.java
+++ src/Dowhile.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Dowhile {
   public static void main(String[] args) {
+    /*
     Scanner scanner = new Scanner(System.in);
     System.out.println("Введите 5:");
     int value = scanner.nextInt();
@@ -13,6 +14,14 @@ public class Dowhile {
       System.out.println("Введите 5:");
       value = scanner.nextInt();
     }
+    System.out.println("Ура! Вы ввели 5!"); //В данной программе присутствует дублирование кода, что нехорошо с точки зрения правильного синтаксиса
+    */
+    Scanner scanner = new Scanner(System.in);
+    int value;
+    do {
+      System.out.println("Введите 5:");
+      value = scanner.nextInt();
+    } while(value != 5);
     System.out.println("Ура! Вы ввели 5!");
   }
 }
