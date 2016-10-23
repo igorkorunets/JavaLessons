fb90cf9adbea1cdb42f9b0ecb21a14a92c5aece6
Igor Korunets <igor.korounets@gmail.com>
2016-10-23 16:21:54 +0300

    ClassesAndObjects - 1change

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -7,11 +7,11 @@ public class ClassesAndObjects {
     Person person1 = new Person();
     person1.name = "Роман";
     person1.age = 50;
-    System.out.println("Меня зовут " + person1.name + ", " + "мне " + person1.age + "лет.");
+    System.out.println("Меня зовут " + person1.name + ", " + "мне " + person1.age + " лет.");
     Person person2 = new Person();
     person2.name = "Вова";
     person2.age = 20;
-    System.out.println("Меня зовут " + person2.name + ", " + "мне " + person2.age + "лет.");
+    System.out.println("Меня зовут " + person2.name + ", " + "мне " + person2.age + " лет.");
   }
 }
 
