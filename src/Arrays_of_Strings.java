5500bbb45ae94ebdb54b38c1f90ffed30c420ec7
Igor Korunets <igor.korounets@gmail.com>
2016-10-23 13:18:21 +0300

    add Arrays_of_Strings

---
+++ src/Arrays_of_Strings.java
@@ -0,0 +1,12 @@
+/*
+Project name: "Массивы в Java"
+21.10.2016
+*/
+public class Arrays_of_Strings {
+  public static void main(String[] args) {
+    int[] numbers = new int[5];
+    numbers[0] = 10;
+    System.out.println(numbers[0]);
+    System.out.println(numbers[1]);
+  }
+}
