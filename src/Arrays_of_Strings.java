5efc516272bee6d550973233483050a9c837d387
Igor Korunets <igor.korounets@gmail.com>
2016-10-23 13:33:22 +0300

    Arrays_of_Strings - 1change

--- src/Arrays_of_Strings.java
+++ src/Arrays_of_Strings.java
@@ -6,7 +6,11 @@ public class Arrays_of_Strings {
   public static void main(String[] args) {
     int[] numbers = new int[5];
     numbers[0] = 10;
-    System.out.println(numbers[0]);
-    System.out.println(numbers[1]);
+    String[] strings = new String[3];
+    strings[0] = "Привет";
+    strings[1] = "Пока";
+    strings[2] = "Джава";
+    System.out.println(strings[0]);
+    System.out.println(strings[2]);
   }
 }
