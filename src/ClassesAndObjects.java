711cc6e0e65b4e9830c25afb1ac47073995182c3
Igor Korunets <igor.korounets@gmail.com>
2016-10-23 16:29:03 +0300

    ClassesAndObjects - 2change

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -7,11 +7,12 @@ public class ClassesAndObjects {
     Person person1 = new Person();
     person1.name = "Роман";
     person1.age = 50;
-    System.out.println("Меня зовут " + person1.name + ", " + "мне " + person1.age + " лет.");
+    person1.speak();
+    person1.sayHello();
     Person person2 = new Person();
     person2.name = "Вова";
     person2.age = 20;
-    System.out.println("Меня зовут " + person2.name + ", " + "мне " + person2.age + " лет.");
+    person2.speak();
   }
 }
 
@@ -21,4 +22,11 @@ class Person {
   //2. Действия, которые он может совершать (методы)
   String name;
   int age;
+
+  void speak() {
+    System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
+  }
+  void sayHello() {
+    System.out.println("Привет!");
+  }
 }
