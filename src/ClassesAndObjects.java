d72c1756d43fdfd32251090c0c5b247921ea9b14
Igor Korunets <igor.korounets@gmail.com>
2016-10-26 18:43:15 +0300

    ClassesAndObjects - 5change

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,12 +1,11 @@
 /*
-Project name: "Классы и объекты" , "Тип возвращаемого значения метода"
-23.10.2016, 26.10.2016
+Project name: "Классы и объекты", "Тип возвращаемого значения метода", "Параметры метода"
+23.10.2016, 26.10.2016, 26.10.2016
 */
 public class ClassesAndObjects {
   public static void main(String[] args) {
     Person person1 = new Person();
-    person1.name = "Роман";
-    person1.age = 50;
+    person1.setNameAndAge("Роман", 50);
     person1.sayHello();
     person1.speak();
     int year1 = person1.calculateYears();
@@ -31,6 +30,11 @@ class Person {
   //2. Действия, которые он может совершать (методы)
   String name;
   int age;
+
+  void setNameAndAge(String username, int userage) {
+    name = username;
+    age = userage;
+  }
 
   int calculateYears() { //Подсчет оставшихся лет до пенсии
     int years = 65 - age;
