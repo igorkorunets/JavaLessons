2b010c19f1814e9a44e12deb61823f523aa5990f
Igor Korunets <igor.korounets@gmail.com>
2016-10-26 17:06:00 +0300

    ClassesAndObjects - 3change

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,18 +1,25 @@
 /*
 Project name: "Классы и объекты"
-23.10.2016
+23.10.2016, 26.10.2016
 */
 public class ClassesAndObjects {
   public static void main(String[] args) {
     Person person1 = new Person();
     person1.name = "Роман";
     person1.age = 50;
-    person1.speak();
     person1.sayHello();
+    person1.speak();
+    person1.calculateYears();
+
+    System.out.println("___________________________");
+    System.out.println();
+
     Person person2 = new Person();
     person2.name = "Вова";
     person2.age = 20;
+    person1.sayHello();
     person2.speak();
+    person2.calculateYears();
   }
 }
 
@@ -23,6 +30,10 @@ class Person {
   String name;
   int age;
 
+  void calculateYears() { //Подсчет оставшихся лет до пенсии
+    int years = 65 - age;
+    System.out.println("Количестов лет до пенсии: " + years);
+  }
   void speak() {
     System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
   }
